f57d100a1e05b0027e0d484cca1e7c3c068816d9
VonMuck <gregoire.muckensturm@gmail.com>
2026-09-08 15:59:57 +0200

    Random image stops onclick

--- FrontPageFunction.js
+++ FrontPageFunction.js
@@ -29,6 +29,49 @@ randomImage.style.display='block';
 }
 
 function srcRandomizer(){
+let x = Math.floor((Math.random()*46)+1);
+let randomSource = "./Zine/123img/" + x + ".jpg";
+document.getElementById('randomImage').src = randomSource;
+}
+
+/* https://stackoverflow.com/questions/4564251/change-the-mouse-pointer-using-javascript */
+/* function changeCursor(){
+document.getElementsByTagName("body")[0].style.cursor="url('./FrontPageVisu/SwordCursor.cur'), default";
+} */
+
+/* var elementToChange=document.getElementsByTagName("homePageGrid")[0];
+elementToChange.style.cursor="url('./FrontPageVisu/SwordCursor.cur'), default"; */
+function changeCursor(){
+  const cursorImage=document.getElementById('sword');
+  if (cursorImage.src.match("Sword"))
+  {
+  cursorImage.src="./FrontPageVisu/DefaultCursor.png";
+  document.body.style.cursor="url('./FrontPageVisu/SwordCursor.cur'), default";
+  document.body.style.cursor.pointer="url('./FrontPageVisu/FairyCursor.cur'), pointer";
+  }
+  else
+  {
+  cursorImage.src="./FrontPageVisu/Sword.png";
+  document.body.style.cursor="default";
+  }
+} 
+
+function displayRandomImage(){
+const disclaimer=document.getElementById('disclaimer');
+const randomImage=document.getElementById('randomImage');
+if (randomImage.style.display.match('block'))
+{
+randomImage.style.display='none';
+disclaimer.style.display='block';
+}
+else
+{
+disclaimer.style.display='none';
+randomImage.style.display='block';
+}
+}
+
+function srcRandomizer(){
 let x = Math.floor((Math.random()*47)+1);
 let randomSource = "./Zine/123img/" + x + ".jpg";
 console.log(randomSource);
