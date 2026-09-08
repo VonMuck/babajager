fc5774d411a263b0ed25b6ee3ef5d467a90badcb
VonMuck <gregoire.muckensturm@gmail.com>
2026-09-08 14:40:11 +0200

    Add files via upload

---
+++ Diary/DiaryFunction.js
@@ -0,0 +1,48 @@
+function displayMusic(){
+ const displayMusic = document.getElementById('diaryMusic');
+ if (displayMusic.style.display.match("grid"))
+ {
+ displayMusic.style.display='none';
+ }
+ else
+ {
+ displayMusic.style.display='grid';
+ }
+}
+
+
+function displayVideogames(){
+ const displayVG = document.getElementById('diaryVideoGames');
+ if (displayVG.style.display.match("grid"))
+ {
+ displayVG.style.display='none';
+ }
+ else
+ {
+ displayVG.style.display='grid';
+ }
+}
+
+function displayBooks(){
+ const displayBook = document.getElementById('diaryBooks');
+ if (displayBook.style.display.match("grid"))
+ {
+ displayBook.style.display='none';
+ }
+ else
+ {
+ displayBook.style.display='grid';
+ }
+}
+
+function displayMovies(){
+ const displayBook = document.getElementById('diaryMovies');
+ if (displayBook.style.display.match("grid"))
+ {
+ displayBook.style.display='none';
+ }
+ else
+ {
+ displayBook.style.display='grid';
+ }
+}
